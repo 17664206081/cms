8aad95eacf7bc549dae7c31479fc96720c6ca61b
fzy <fzy@choicesoft.com.cn>
2018-11-07 10:29:42 +0800

    配置ssl

--- README.md
+++ README.md
@@ -14,4 +14,11 @@
 #说明
 运行本项目前必须安装gradle
 
+#ssl生成
+1. 在当前项目下打开cmd
+```
+keytool -genkey -alias tomcat -keyalg RSA -keystore ./server.keystore 
+``` 
+![图片暂时无法显示]("https://github.com/17664206081/cms/blob/dev/img-storage/menu.saveimg.savepath20181107101411.jpg")
+
 
--- cms-web/src/main/java/com/fzy/config/SSLConfig.java
+++ cms-web/src/main/java/com/fzy/config/SSLConfig.java
@@ -1,10 +1,65 @@
 package com.fzy.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @program: SSLConfig
  * @description:
  * @author: fzy
  * @date: 2018-11-07 09:56
  **/
+@Configuration
 public class SSLConfig {
+
+    @Bean
+    public Connector connector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer(){
+        TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector());
+        return tomcat;
+    }
+
+  /*  @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }*/
+
 }
